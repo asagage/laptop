381767a63f5787b4fefe14ed9a25eb6826423033
Asa Gage <asagage@gmail.com>
2019-05-04 11:26:47 -0400

    merging content from geerlingguys mac-dev-playbook

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-# Welcome to the siyelo laptop script!
-# Be prepared to turn your OSX box into 
-# a development beast.
-#
-# This script bootstraps our OSX laptop to a point where we can run
+# This script bootstraps an OSX laptop to a point where we can run
 # Ansible on localhost. It;
 #  1. Installs 
 #    - xcode
-#    - homebrew
-#    - ansible (via brew) 
-#    - a few ansible galaxy playbooks (zsh, homebrew, cask etc)  
+#    - ansible (via pip) 
+#    - a few ansible galaxy playbooks  
 #  2. Kicks off the ansible playbook
 #    - main.yml
 #
 # It will ask you for your sudo password
+
+# base install path
+basedir='~/dev/asagage'
+
+# current dir
+cwd=$(pwd)
 
 fancy_echo() {
   local fmt="$1"; shift
@@ -42,36 +43,46 @@ else
   fancy_echo "Xcode already installed. Skipping."
 fi
 
-if ! command -v brew >/dev/null; then
-  fancy_echo "Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
-else
-  fancy_echo "Homebrew already installed. Skipping."
-fi
+# setup our base dir
+fancy_echo("Creating base dir if needed ...")
+mkdir -p $basedir
 
-# [Install Ansible](http://docs.ansible.com/intro_installation.html).
-if ! command -v ansible >/dev/null; then
-  fancy_echo "Installing Ansible ..."
-  brew install ansible 
-else
-  fancy_echo "Ansible already installed. Skipping."
-fi
-
-# Clone the repository to your local drive.
-if [ -d "./laptop" ]; then
+if [ -d "$basedir/laptop" ]; then
   fancy_echo "Laptop repo dir exists. Removing ..."
-  rm -rf ./laptop/
+  rm -rf $basedir/laptop/
 fi
 fancy_echo "Cloning laptop repo ..."
 git clone https://github.com/asagage/laptop.git 
 
 fancy_echo "Changing to laptop repo dir ..."
-cd laptop
+cd $basedir/laptop
+
+# Install pip.
+fancy_echo "Installing pip ..."
+sudo easy_install pip
+
+# Install Ansible.
+fancy_echo "Installing Ansible ..."
+sudo pip install ansible
+
+# Add ansible.cfg to pick up roles path.
+{ echo '[defaults]'; echo 'roles_path = ../'; } >> ansible.cfg
+
+# Add an ansible hosts file.
+sudo mkdir -p /etc/ansible
+sudo touch /etc/ansible/hosts
+echo -e '[local]\nlocalhost ansible_connection=local' | sudo tee -a /etc/ansible/hosts > /dev/null
 
 # Install Roles
 fancy_echo "Installing galaxy roles ..."
 ansible-galaxy install --roles-path ./roles -r requirements.yml
 
+# Check the role/playbook's syntax.
+fancy_echo "Checking playbook syntax ..."
+ansible-playbook main.yml --syntax-check
+
 # Run this from the same directory as this README file. 
 fancy_echo "Running ansible playbook ..."
-ansible-playbook playbook.yml -i hosts -K -vvvv 
+ansible-playbook playbook.yml -i hosts -K -vvv 
+
+fancy_echo "All set! Enjoy."
